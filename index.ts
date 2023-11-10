859d084b6bdd343517446702b05d33e3b9238029
amir-roshan <amirvalielahi@gmail.com>
2023-11-09 20:06:32 -0800

    send html files to the web sever

--- index.ts
+++ index.ts
@@ -4,8 +4,19 @@ import fs from 'fs';
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end("<h1>Home</h1>");
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), "utf8", (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    }
+
+    if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), "utf8", (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
     }
 });
 
